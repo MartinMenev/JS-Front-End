6888789158054d89add4651a7d22e16304705dcf
MartinMenev <martin.menew@gmail.com>
2023-04-15 14:43:39 +0300

    New task

---
+++ JS-Front-End/Functions/addAndSubtract.js
@@ -0,0 +1,12 @@
+function addAndSubtract (...numbers) {
+    return subtract(sum(numbers[0], numbers[1]), numbers[2]);
+
+    function sum (a, b) {
+        return a + b;
+    }
+
+    function subtract (a, b) {
+        return a - b;
+    }
+}
+
