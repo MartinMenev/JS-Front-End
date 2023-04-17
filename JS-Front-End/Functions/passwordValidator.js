0c3fdc37eba949334420c416aa49c86d3c35eff0
MartinMenev <martin.menew@gmail.com>
2023-04-17 17:29:35 +0300

    Formatted

--- JS-Front-End/Functions/passwordValidator.js
+++ JS-Front-End/Functions/passwordValidator.js
@@ -1,4 +1,4 @@
-function passwordValidate (input) {
+function passwordValidate(input) {
 
     if (checkContent(input) && containsNumber(input) && checkIf2digits(input)) {
         console.log(`Password is valid`);
@@ -16,34 +16,34 @@ function passwordValidate (input) {
     }
 
     function checkValidLength(text) {
-       return text.length >= 6 && text.length <= 10;
+        return text.length >= 6 && text.length <= 10;
     }
 
     function onlyLettersAndNumbers(str) {
         return /^[A-Za-z0-9]*$/.test(str);
-      }
+    }
 
     function checkContent(text) {
-    
-       return onlyLettersAndNumbers(text);
+
+        return onlyLettersAndNumbers(text);
     }
 
     function containsNumber(str) {
         return /\d/.test(str);
-      }
+    }
 
     function checkIf2digits(text) {
-       let count = 0;
-       text.split("").forEach(element => {
-        if (containsNumber(element)) {
-            count++;
-        }
-       });
+        let count = 0;
+        text.split("").forEach(element => {
+            if (containsNumber(element)) {
+                count++;
+            }
+        });
 
-       if (count >= 2) {
-        return true;
-       } else {
-        return false;
-       }
+        if (count >= 2) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
